30bb08f06dea92be9d716916ef591f3bd69a1618
Alexis Ramos <alexis.ramosf@gmail.com>
2020-10-01 22:29:36 +0200

    Creado esqueleto del proyecto

---
+++ StockService/src/main/java/es/arf/mca/tfm/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package es.arf.mca.tfm.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
